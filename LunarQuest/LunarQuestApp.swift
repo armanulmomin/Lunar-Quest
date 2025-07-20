0f96795e1928afd8fff728e5004304ce902b2781
Armanul Momin <armanul.momin@gmail.com>
2025-07-20 19:10:33 +1000

    first Commit

---
+++ LunarQuest/LunarQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunarQuestApp.swift
+//  LunarQuest
+//
+//  Created by Arman on 20/7/25.
+//
+
+import SwiftUI
+
+@main
+struct LunarQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
